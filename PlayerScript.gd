8e586f768f75f317054d25f4ecbe78e72b92aa53
Milica <psymp12@nottingham.ac.uk>
2022-02-12 22:03:04 +0000

    move_and_slide is very annoying lol

--- PlayerScript.gd
+++ PlayerScript.gd
@@ -1,25 +1,26 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-export var ACCELERATION = 72
-export var MAXSPEED = 150
-export var FRICTION = 50
-export var JUMPHEIGHT = -500
+export var ACCELERATION = 50
+export var MAXSPEED = 75
+export var FRICTION = 500
+export var JUMPHEIGHT = -300
+export var MAXJUMP = -40
 export var GRAVITY = 10
 
 func _physics_process(delta):
 	var inVel = Vector2.ZERO
-	inVel.y += GRAVITY
 	
 	inVel.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if Input.is_action_just_pressed("ui_accept"):
 		inVel.y = JUMPHEIGHT
 	
+	inVel.y += GRAVITY
 	if inVel != Vector2.ZERO:
 		velocity += inVel * ACCELERATION * delta
 		print("current velocity: ", velocity)
 		velocity = velocity.clamped(MAXSPEED)
-		velocity = move_and_slide(velocity)
+		velocity = move_and_slide(velocity, Vector2(0, -1), 1)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
