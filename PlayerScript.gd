667a25069682fd08a092a2fb78a7666211b835c0
Milica <psymp12@nottingham.ac.uk>
2022-02-12 23:35:11 +0000

    fixed jumping, added a colour filter

--- PlayerScript.gd
+++ PlayerScript.gd
@@ -4,8 +4,7 @@ var velocity = Vector2.ZERO
 export var ACCELERATION = 50
 export var MAXSPEED = 75
 export var FRICTION = 500
-export var JUMPHEIGHT = -300
-export var MAXJUMP = -40
+export var JUMPHEIGHT = -350
 export var GRAVITY = 10
 
 func _physics_process(delta):
@@ -13,14 +12,19 @@ func _physics_process(delta):
 	
 	inVel.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		inVel.y = JUMPHEIGHT
 	
-	inVel.y += GRAVITY
+	#inVel.y += GRAVITY
 	if inVel != Vector2.ZERO:
 		velocity += inVel * ACCELERATION * delta
 		print("current velocity: ", velocity)
 		velocity = velocity.clamped(MAXSPEED)
-		velocity = move_and_slide(velocity, Vector2(0, -1), 1)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
+	
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y += GRAVITY
